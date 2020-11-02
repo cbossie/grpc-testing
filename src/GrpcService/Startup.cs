0ea952e57deb537ff11ea0fa5a4cae437ed70175
cbossie <cbossie@gmail.com>
2020-11-01 21:07:47 -0500

    Updates

--- src/GrpcService/Startup.cs
+++ src/GrpcService/Startup.cs
@@ -37,7 +37,7 @@ namespace GrpcService
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGrpcService<GreeterService>();
-
+                endpoints.MapCodeFirstGrpcReflectionService();
                 endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
